295fda93ba8701658e7b041df4c0285037e7a401
Vincent Klimjack <vklimjack@gmail.com>
2023-11-11 09:01:21 +0000

    add annotate gem

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: kittens
+#
+#  id         :integer          not null, primary key
+#  age        :integer
+#  cuteness   :integer
+#  name       :string
+#  softness   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Kitten < ApplicationRecord
 end
--- test/models/kitten_test.rb
+++ test/models/kitten_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: kittens
+#
+#  id         :integer          not null, primary key
+#  age        :integer
+#  cuteness   :integer
+#  name       :string
+#  softness   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class KittenTest < ActiveSupport::TestCase
